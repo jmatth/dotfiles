35debd9e16a184d1d369e1eecb884b406b1aa176
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-08-06 11:45:53 -0400

    major overhaul
    removing unused files
    
    rewriting link script

--- link.sh
+++ link.sh
@@ -1,47 +1,50 @@
 #!/bin/bash
 
+# Script configs
+IGNORE="bashrc|ssh|link|gitignore"
+
+# Get current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if ! [ -z $1 ]
 then
 	if [ -f ~/dotfiles/$1_bashrc ]
 	then
 		echo "Linking for $1 machine."
-		rm -f ~/.bashrc
-		ln -s ~/dotfiles/$1_bashrc ~/.bashrc
+		unlink ~/.bashrc &> /dev/null
+		unlink ~/.bash_profile &> /dev/null
+		echo "source $DIR/bashrc" > ~/.bashrc
+		echo "source $DIR/$1_bashrc" >> ~/.bashrc
 	else
 		echo "That bashrc doesn't exist..."
 		exit 1
 	fi
 else
 	echo "Linking for personal machine."
-	rm -f ~/.bashrc
-	ln -s ~/dotfiles/bashrc ~/.bashrc
+	echo "source $DIR/bashrc" > ~/.bashrc
 fi
 
-pushd ~/ > /dev/null
-
-# remove old files or links
-rm -f .bash_profile
-rm -f .gitconfig
-rm -f .vimrc
-rm -f .tmux.conf
-
-popd > /dev/null
-
-ln -s ~/.bashrc ~/.bash_profile
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-echo "Linking vim inkpot theme"
-mkdir -p ~/.vim/colors
-rm -f ~/.vim/colors/inkpot.vim
-ln -s ~/dotfiles/inkpot.vim ~/.vim/colors/
+unlink ~/.bash_profile &> /dev/null
+echo "source ~/.bashrc" > ~/.bash_profile
 
 if ! [ -f ~/.ssh/config ]
 then
 	echo "No ssh config found, installing base."
-	mkdir -p ~/.ssh
-	cp ~/dotfiles/ssh_config ~/.ssh/config
-	chmod 744 ~/.ssh/config
+	cp $DIR/ssh_config ~/.ssh/config
 fi
 
-exit 0
+echo "Symlinking all other config files:"
+cd $DIR
+for file in $(git ls-files | egrep -v $IGNORE)
+do
+	echo $file
+	if test ! -d `dirname ~/.$file`
+	then
+		mkdir -p `dirname ~/.$file`
+	fi
+	if test -h ~/.$file
+	then
+		unlink ~/.$file
+	fi
+	ln -sf $DIR/$file ~/.$file
+done
