9e33836d6a12bdb26d59e1372fbc1ce3fd3a59ee
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-08-21 17:45:27 -0400

    fixing link script

--- link.sh
+++ link.sh
@@ -63,12 +63,17 @@ if ! [ -f .git/hooks/post-merge ]
 then
 	echo "Installing post merge hook"
 	hook=".git/hooks/post-merge"
-	echo "cd $DIR" > $hook
+	echo "#!/usr/bin/env bash" > $hook
+	echo "cd $DIR" >> $hook
 	echo "git submodule init && git submodule update" >> $hook
-	if [ -z $1 ]
+	if ! [ -z $1 ]
 	then
 		echo "./link.sh $1" >> $hook
 	else
 		echo "./link.sh" >> $hook
+	fi
 	chmod 755 $hook
+
+	echo "Assuming submodules are empty, initializing now"
+	git submodule init && git submodule update
 fi
