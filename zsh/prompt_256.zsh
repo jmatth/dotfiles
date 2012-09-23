dcbd1821aeacb9a8d22c2ea5bc93c29506e90ba1
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-09-22 23:27:51 -0400

    new fancy zsh prompt

--- zsh/prompt_256.zsh
+++ zsh/prompt_256.zsh
@@ -6,23 +6,23 @@ local nc="%{$FX[reset]%}"
 
 # Use different colors if root
 if id | cut -d' ' -f1 | grep -i 'root' 2>&1 >/dev/null; then
-	local name="%{$FX[reset]$FG[bold]$FG[001]%}%n"
+	local name="%{$FX[reset]$FX[bold]$FG[001]%}%n"
 else
-	local name="%{$FX[reset]$FG[bold]$FG[027]%}%n"
+	local name="%{$FX[reset]$FX[bold]$FG[027]%}%n"
 fi
 
 # And different colors if over ssh
 if (($+SSH_CONNECTION)); then
-	local host="%{$FX[reset]$FG[bold]$FG[208]%}%m"
+	local host="%{$FX[reset]$FX[bold]$FG[208]%}%m"
 else
-	local host="%{$FX[reset]$FG[bold]$FG[034]%}%m"
+	local host="%{$FX[reset]$FX[bold]$FG[034]%}%m"
 fi
 
 local jobs="%1(j.(%{$FX[reset]$FG[197]%}%j job%2(j.s.)${p})-.)"
-local time="%{$FX[reset]$FG[215]%}%*"
-local dir="%{$FX[reset]$FG[bold]$FG[045]%}%~"
+local time="%{$FX[reset]$FG[005]%}%*"
+local dir="%{$FX[reset]$FG[045]%}%~"
 
-local last="%(?..%{$FX[reset]$FG[203]%}%??${p}:)"
+local return="%(?.%{$FX[reset]$FG[010]%}☺.%{$FX[reset]$FG[009]%}☹%?)${nc}"
 local hist="%{$FX[reset]$FG[220]%}%!!"
 local priv="%{$FX[reset]%}%#"
 
@@ -32,12 +32,16 @@ zstyle ':vcs_info:*' check-for-changes true
 #zstyle ':vcs_info:*' unstagedstr "%{$FX[reset]$FG[160]%}"
 zstyle ':vcs_info:*' stagedstr "%{$FX[reset]$FG[082]%}⚛"
 zstyle ':vcs_info:*' unstagedstr "%{$FX[reset]$FG[160]%}⚡"
-zstyle ':vcs_info:*' formats "%{$FX[reset]$FG[220]%}[%b%u%c%{$FX[reset]$FG[220]%}]"
+zstyle ':vcs_info:*' formats "%{$FX[reset]$FG[220]%}[%b%u%c%{$FX[reset]$FG[220]%}] "
 
 local vcsi="\${vcs_info_msg_0_}"
+
+PROMPT="${nc}${name}${nc}@${host}${nc}:${nc}${priv} ${nc}"
+RPROMPT="${dir} ${return} ${vcsi}${time}${nc}"
 
 #PROMPT="${p}(${name}${p}@${host}${p})-${jobs}(${time}${p})-(${dir}${p}${vcsi}${p})
 #(${last}${p}${hist}${p}:${priv}${p})- %{$FX[reset]%}"
 
 #Trying to emulate my old bash prompt
-PROMPT="${nc}${name}${nc}@${host}${nc}:${dir}${nc}${vcsi}${nc}${priv} ${nc}"
+#PROMPT="${nc}${name}${nc}@${host}${nc}:${dir}${nc}${vcsi}${nc}${priv} ${nc}"
+#RPROMPT="${last} ${time}${nc}"
