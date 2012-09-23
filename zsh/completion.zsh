98ea2721c27c0d7f1ecec98f19ca2ec6c1c57eaf
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-09-22 22:22:42 -0400

    changing zsh completion cache location

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -9,5 +9,6 @@ zstyle ':completion:*' menu select
 zstyle ':completion:*' list-colors "${LS_COLORS}" # Complete with same colors as ls.
 zstyle ':completion:*' max-errors 2 # Be lenient to 2 errors.
 zstyle ':completion:*' use-cache true # Use a completion cache.
+zstyle ':completion:*' cache-path ~/.zsh/cache # Keep the completion cache in my .zsh directory.
 zstyle ':completion:*' ignore-parents pwd # Ignore the current directory in completions.
 zstyle ':completion:*' squeeze-slashes true # Remove trailing directory slashes
