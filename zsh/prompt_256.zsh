ed14c428ce58c66f4cc6faa2bbf67131c257ddcd
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-09-27 22:28:31 -0400

    cleaning up 256 color prompt

--- zsh/prompt_256.zsh
+++ zsh/prompt_256.zsh
@@ -6,38 +6,36 @@ local nc="%{$FX[reset]%}"
 
 # Use different colors if root
 if id | cut -d' ' -f1 | grep -i 'root' 2>&1 >/dev/null; then
-	local name="%{$FX[reset]$FX[bold]$FG[001]%}%n"
+	local name="%{$FX[bold]$FG[001]%}%n${nc}"
 else
-	local name="%{$FX[reset]$FX[bold]$FG[027]%}%n"
+	local name="%{$FX[bold]$FG[027]%}%n$nc"
 fi
 
 # And different colors if over ssh
 if (($+SSH_CONNECTION)); then
-	local host="%{$FX[reset]$FX[bold]$FG[208]%}%m"
+	local host="%{$FX[bold]$FG[208]%}%m${nc}"
 else
-	local host="%{$FX[reset]$FX[bold]$FG[034]%}%m"
+	local host="%{$FX[bold]$FG[034]%}%m${nc}"
 fi
 
-local jobs="%1(j.(%{$FX[reset]$FG[197]%}%j job%2(j.s.)${p})-.)"
-local time="%{$FX[reset]$FG[005]%}%*"
-local dir="%{$FX[reset]$FG[045]%}%~"
+local jobs="%1(j.(%{$FG[197]%}%j job%2(j.s.)${p})-.)${nc}"
+local time="%{$FG[005]%}%*$nc"
+local dir="%{$FG[045]%}%~${nc}"
 
-local return="%(?.%{$FX[reset]$FG[010]%}☺.%{$FX[reset]$FG[009]%}☹%?)${nc}"
-local hist="%{$FX[reset]$FG[220]%}%!!"
-local priv="%{$FX[reset]%}%#"
+local return="%(?.%{$FG[010]%}☺.%{$FG[009]%}☹%?)${nc}"
+local hist="%{$FG[220]%}%!!${nc}"
+local priv="%#"
 
 # Use zshcontrib's vcs_info to get information about any current version control systems.
 zstyle ':vcs_info:*' check-for-changes true
-#zstyle ':vcs_info:*' stagedstr "%{$FX[reset]$FG[082]%}"
-#zstyle ':vcs_info:*' unstagedstr "%{$FX[reset]$FG[160]%}"
-zstyle ':vcs_info:*' stagedstr "%{$FX[reset]$FG[082]%}⚛"
-zstyle ':vcs_info:*' unstagedstr "%{$FX[reset]$FG[160]%}⚡"
-zstyle ':vcs_info:*' formats "%{$FX[reset]$FG[220]%}[%b%u%c%{$FX[reset]$FG[220]%}] "
+zstyle ':vcs_info:*' stagedstr "%{$FG[082]%}⚛${nc}"
+zstyle ':vcs_info:*' unstagedstr "%{$FG[160]%}⚡${nc}"
+zstyle ':vcs_info:*' formats "%{$FG[220]%}[%b%u%c%{$FG[220]%}]${nc} "
 
 local vcsi="\${vcs_info_msg_0_}"
 
-PROMPT="${nc}${name}${nc}@${host}${nc}:${nc}${priv} ${nc}"
-RPROMPT="${dir} ${return} ${vcsi}${time}${nc}"
+PROMPT="${name}@${host}:${priv} "
+RPROMPT="${dir} ${return} ${vcsi}${time}"
 
 #PROMPT="${p}(${name}${p}@${host}${p})-${jobs}(${time}${p})-(${dir}${p}${vcsi}${p})
 #(${last}${p}${hist}${p}:${priv}${p})- %{$FX[reset]%}"
